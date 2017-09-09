ab3ddb3bdadc468b41e64bbaa38df4786b7cec6c
方雨_Yu <304683191@qq.com>
2017-09-09 21:08:39 +0800

    增加 示例使用的Icon图标

--- src/components/icon/svg.js
+++ src/components/icon/svg.js
@@ -1,4 +1,5 @@
 import '@/assets/svg/loading.svg';
+import '@/assets/svg/loading-light.svg';
 import '@/assets/svg/all.svg';
 import '@/assets/svg/cart.svg';
 import '@/assets/svg/back.svg';
@@ -7,3 +8,8 @@ import '@/assets/svg/comments.svg';
 import '@/assets/svg/search.svg';
 import '@/assets/svg/set.svg';
 import '@/assets/svg/refresh.svg';
+import '@/assets/svg/left.svg';
+import '@/assets/svg/ellipsis.svg';
+import '@/assets/svg/close.svg';
+import '@/assets/svg/scan.svg';
+import '@/assets/svg/microphone.svg';
