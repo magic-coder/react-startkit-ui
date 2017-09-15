27a50693deb2551532e20df385098c843d23ee66
方雨_Yu <304683191@qq.com>
2017-09-15 22:54:20 +0800

    优化 404 页面

--- src/views/404/NotFound.jsx
+++ src/views/404/NotFound.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './scss';
 
 export default class NotFound extends React.Component {
   render() {
     return (
-      <div className="page page__not__found">404</div>
+      <div className="page page__not__found">
+        <section>
+          <h1>404</h1>
+          <p>你要找的页面不存在，<Link to="/">返回首页</Link></p>
+        </section>
+      </div>
     );
   }
 }
